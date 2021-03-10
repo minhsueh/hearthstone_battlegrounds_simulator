332bccd7b8d63e4ba327c7a211b87608ef70a2ae
Min-Hsueh chiu <71813684+minhsueh@users.noreply.github.com>
2021-03-10 04:18:33 -0800

    Update battlegrounds.py

--- battlegrounds.py
+++ battlegrounds.py
@@ -16,16 +16,8 @@ card_pool:
 from cardpool import *
 import random
 import copy
-
-class minion_skill_round_start_effect():
-
-    def RedWhelp(A_deck, B_deck):
-        dmg = 0
-        for i in A_deck:
-            if i.type == 'dragon':
-                dmg += 1
-        random.choice(B_deck).blood -= dmg
-
+import sys
+from copy import deepcopy
 
 
 
@@ -35,7 +27,7 @@ class deck():
         self.max_deck_length = 7
 
     def add(self, minion):
-        self.deck.append(minion)
+        self.deck.append(deepcopy(minion))
 
     def death(self,minion_position):
         self.deck.pop(minion_position)
@@ -69,6 +61,10 @@ class deck():
                 effect_list.append(i)
         return(effect_list)
 
+    def update_position(self):
+        for i in range(len(self.deck)):
+            self.deck[i].position = i 
+
     def check_attacker_position(self):
         if not self.deck:
             return(0)
@@ -89,8 +85,8 @@ class deck():
     def check_multiple_summon(self):
         base = 1
         for i in self.deck:
-            if i.skill['multiple_summon']:
-                base = max(base, i.skill['multiple_summon'])
+            if i.skill.get('multiple_summon', False):
+                base *= i.skill['multiple_summon']
         return(base)
 
     def summon(self, summoner_position, minion):
@@ -98,9 +94,17 @@ class deck():
         counter = 1
         while(counter <= N_summon):
             if not self.check_full_deck():
-                self.deck.append(summoner_position, minion)
+                self.deck.insert(summoner_position, deepcopy(minion))
             else:
                 return
+            counter += 1
+
+    ##charge
+    def check_charge(self):
+        for i in range(len(self.deck)):
+            if self.deck[i].charge:
+                return([i])
+        return(False)
 
 
     #deathrattle
@@ -109,7 +113,7 @@ class deck():
             return
         deathrattle_list = []
         for i in range(len(query)):
-            if query[i].deathrattle:
+            if query[i].skill.get('deathrattle', False):
                 deathrattle_list.append(i)
         return(deathrattle_list)
 
@@ -117,20 +121,9 @@ class deck():
     def check_multiple_deathrattle(self):
         base = 1
         for i in self.deck:
-            if i.skill['multiple_deathrattle']:
+            if i.skill.get('multiple_deathrattle', False):
                 base = max(base, i.skill['multiple_deathrattle'])
         return(base)
-
-    def apply_all_deathrattle(self):
-        death_list = self.check_death()
-
-        if not death_list and self.check_deathrattle_exist(death_list):
-            N_deathrattle = self.check_multiple_deathrattle()
-            for i in death_list:
-                counter = 1
-                while(counter <= N_deathrattle):
-                    i.skill['deathrattle']()
-                    counter += 1
 
     def check_death(self):
         death_list = []
@@ -144,19 +137,76 @@ class deck():
                 else:
                     death_list.append(self.deck.pop(N-1))
             N -= 1
+        death_list.reverse()
+        return(death_list)
 
-        return(death_list.reverse())
+    def check_single_minion_death(self, position):
+        if self.deck[position].blood <= 0:
+            self.deck.pop(position)
+            return(True)
+        else:
+            return(False)
 
 
 class battlegrounds():
-    def __init__(self, A_deck, B_deck):
+    def __init__(self, A_deck, B_deck, verbose = False):
         self.A_deck = A_deck
         self.B_deck = B_deck
         self.A_deck_attacker_position = 0
         self.B_deck_attacker_position = 0
 
+        self.verbose = verbose
+
         self.poison_attack = 99999
 
+
+    def update_all_deck_position(self):
+        self.A_deck.update_position()
+        self.B_deck.update_position()
+
+    def apply_round_start_effect(self, attacker, defender):
+        #apply effect
+        for idx in attacker.round_start_effect_exist():
+            attacker.deck[idx].skill['round_start_effect'](attacker.deck[idx], attacker, defender)
+        #check death and deathrattle
+        self.apply_all_deathrattle(defender, attacker)
+        if self.verbose:
+            self.visulization()
+        self.update_all_deck_position()
+            
+
+    def apply_all_deathrattle(self, deathrattle_deck, be_affected_deck):
+        death_list = deathrattle_deck.check_death()
+        if deathrattle_deck.check_deathrattle_exist(death_list):
+            N_deathrattle = deathrattle_deck.check_multiple_deathrattle()
+            for i in death_list:
+                counter = 1
+                while(counter <= N_deathrattle):
+                    i.skill['deathrattle'](i, deathrattle_deck, be_affected_deck) 
+                    
+                    #if having charge
+                    self.update_all_deck_position()
+                    while(deathrattle_deck.check_charge()):
+                        
+                        self.check_win()
+                        self.apply_all_deathrattle(be_affected_deck, deathrattle_deck)
+                        self.update_all_deck_position()
+                        charge_idx = deathrattle_deck.check_charge()[0]
+                        if self.verbose:
+                            self.visulization()
+                        self.minion_combat(deathrattle_deck, be_affected_deck, charge_idx)
+                        if not deathrattle_deck.check_single_minion_death(charge_idx):
+                            deathrattle_deck[charge_idx].charge = False
+                        
+
+                        self.update_all_deck_position()
+                        #self.apply_all_deathrattle(deathrattle_deck, be_affected_deck)
+                        self.apply_all_deathrattle(be_affected_deck, deathrattle_deck) 
+                    self.B_deck_attacker_position = self.B_deck.check_attacker_position()
+                    self.A_deck_attacker_position = self.A_deck.check_attacker_position()
+                    
+
+                    counter += 1
 
     
 
@@ -215,7 +265,20 @@ class battlegrounds():
         ####
         attacker_deck.deck[attacker_position].already_attacked = True
 
-    def combat(self, verbose = False):
+        
+    def check_win(self):
+        if len(self.A_deck.deck) == 0 and len(self.B_deck.deck) == 0:
+            print('tie')
+            sys.exit()
+        elif len(self.A_deck.deck) == 0:
+            print('B')
+            sys.exit()
+        elif len(self.B_deck.deck) == 0:
+            print('A')
+            sys.exit()
+
+
+    def combat(self):
     
         ##determine first attacker
         ##self.attacker = True -----> A
@@ -226,52 +289,62 @@ class battlegrounds():
         else:
             self.attacker = random.choice([True, False])
 
+        if self.verbose:
+            self.visulization()
 
         '''
         start of combat effect
         '''
+        self.update_all_deck_position()
         if self.attacker:
             if self.A_deck.round_start_effect_exist():
-                #apply effect
-                for idx in self.A_deck.round_start_effect_exist():
-                    A_deck[idx].skill['round_start_effect'](A_deck, B_deck)
-                #check death and deathrattle
-                B_deck.apply_all_deathrattle()
-                self.B_deck_attacker_position = B_deck.check_attacker_position()
+                self.apply_round_start_effect(self.A_deck, self.B_deck)
+                self.B_deck_attacker_position = self.B_deck.check_attacker_position()
             
             if self.B_deck.round_start_effect_exist(): 
-                #apply effect
-                for idx in self.B_deck.round_start_effect_exist():
-                    B_deck[idx].skill['round_start_effect'](B_deck, A_deck)
-                #check death and deathrattle
-                A_deck.apply_all_deathrattle()
-                self.A_deck_attacker_position = A_deck.check_attacker_position()
+                self.apply_round_start_effect(self.B_deck, self.A_deck)
+                self.A_deck_attacker_position = self.A_deck.check_attacker_position()
+        else:
+            if self.B_deck.round_start_effect_exist(): 
+                self.apply_round_start_effect(self.B_deck, self.A_deck)
+                self.A_deck_attacker_position = self.A_deck.check_attacker_position()
+            if self.A_deck.round_start_effect_exist():
+                self.apply_round_start_effect(self.A_deck, self.B_deck)
+                self.B_deck_attacker_position = self.B_deck.check_attacker_position()
 
 
+        self.check_win()
         #physical combat start
         while(self.A_deck.check_length() > 0 and self.B_deck.check_length() > 0):
-            if verbose:
+            if self.verbose:
                 self.visulization()
 
             if self.attacker:
                 self.minion_combat(self.A_deck, self.B_deck, self.A_deck_attacker_position)
                 #check death and deathrattle
-                self.A_deck.apply_all_deathrattle()
-                self.B_deck.apply_all_deathrattle() 
+                self.apply_all_deathrattle(self.A_deck, self.B_deck)
+                #self.update_all_deck_position()
+                self.apply_all_deathrattle(self.B_deck, self.A_deck) 
+                #self.update_all_deck_position()
                 #check_attacker_position
                 self.B_deck_attacker_position = self.B_deck.check_attacker_position()
                 self.A_deck_attacker_position = self.A_deck.check_attacker_position()
             else:
                 self.minion_combat(self.B_deck, self.A_deck, self.B_deck_attacker_position)
                 #check death and deathrattle
-                self.B_deck.apply_all_deathrattle()
-                self.A_deck.apply_all_deathrattle()
+                self.apply_all_deathrattle(self.B_deck, self.A_deck) 
+                #self.update_all_deck_position()
+                self.apply_all_deathrattle(self.A_deck, self.B_deck)
+                #self.update_all_deck_position()
                 #check_attacker_position
                 self.A_deck_attacker_position = self.A_deck.check_attacker_position()
                 self.B_deck_attacker_position = self.B_deck.check_attacker_position()
 
             #change attacker from A to B, or B to A
             self.attacker = (not self.attacker)
+
+        if self.verbose:
+            self.visulization()
 
         if self.A_deck.check_length() > 0:
             return('A')
@@ -290,6 +363,8 @@ class battlegrounds():
                 status +='T'
             if i.reborn:
                 status += 'R'
+            if i.skill.get('deathrattle', False):
+                status += 'D'
             vis_list.append((status, i.attack, i.blood))
         return(vis_list)
 
@@ -333,19 +408,24 @@ def main():
     A_deck = deck()
     B_deck = deck()
 
-    A_deck.add(RockpoolHunter)
-    A_deck.add(DragonspawnLieutenant)
-    A_deck.add(SkyPirate)
     
-    B_deck.add(ValgarHomunculus)
-    B_deck.add(AcolyteOfCThun)
-    #B_deck.add(RedWhelp)
+    
+    A_deck.add(Scallywag)
+    #A_deck.add(Scallywag)
+    #A_deck.add(RockpoolHunter)
+    #A_deck.add(AcolyteOfCThun)
+    
+    #B_deck.add(ValgarHomunculus)
+    #B_deck.add(ValgarHomunculus)
+    #B_deck.add(AcolyteOfCThun)
+    B_deck.add(RedWhelp)
     
 
 
-    #bg = battlegrounds(A_deck, B_deck)
-    cc = combat_calculator()
-    cc.calculate(A_deck, B_deck)
+    bg = battlegrounds(A_deck, B_deck, verbose = True)
+    print(bg.combat())
+    #cc = combat_calculator()
+    #cc.calculate(A_deck, B_deck)
 
 
 
